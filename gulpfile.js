930a70c66ee28c88f556118607a57a67e91d81e5
Paweł Sokołowski <pawel.sokolowski@outlook.com>
2015-11-04 22:21:49 +0100

    lepszy sposób na zapewnienie scalanie plików modułów w pierwszej kolejności

--- gulpfile.js
+++ gulpfile.js
@@ -11,15 +11,12 @@ gulp.task('default', function() {
 gulp.task('build-js', function() {
   return gulp
     .src([
-        'app/app.js',
-        'app/view1/zview1-module.js',
-        'app/view2/view2-module.js',
+        'app/**/*-module.js',
         'app/**/*.js', 
         '!app/bower_components/**', 
         '!app/**/*_test.js'])
     .pipe(sourcemaps.init())
     .pipe(concat('app.js'))
-      //only uglify if gulp is ran with '--type production'
     .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) 
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'));
