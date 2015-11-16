cd9be2f230e8213c439589bc13bfbddff56a8354
Paweł Sokołowski <pawel.sokolowski@outlook.com>
2015-11-16 22:14:43 +0100

    Użycie localStorage

--- app/albums/albumsService.js
+++ app/albums/albumsService.js
@@ -1,21 +1,37 @@
+/*global angular*/
+
 (function(){			
+	const albumsKey = 'ms.albums';
+	
 	class AlbumsService{
-		constructor($http){
+		constructor($http, $q){
+			this._$q = $q;
 			this._$http = $http;
 		}
 		
 		getByGenre(genre){
+			return this._getAlbums()
+				.then(albums => albums.filter(a => a.category == genre))
+		}
+		
+		_getAlbums(){
+			let albums = localStorage[albumsKey];
+			if(albums){
+				let deferredAlbums = this._$q.defer();
+				deferredAlbums.resolve(JSON.parse(albums));
+				return deferredAlbums.promise;
+			}
 			return this._$http
 				.get('data/albums.json')
-				.then(response => filterCategories(response.data));
-				
-			function filterCategories(albums){
-				return albums.filter(a => a.category == genre);
-			}
+				.then(response => {
+					localStorage[albumsKey] = JSON.stringify(response.data);
+					return response.data;
+				});
 		}
 	}
 	
-	AlbumsService.$inject = ['$http'];
+	
+	AlbumsService.$inject = ['$http', '$q'];
 	
 	angular
 		.module("app.albums")
